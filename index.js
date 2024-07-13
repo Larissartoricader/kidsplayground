96c364e1535cbd116e35049120752727eac656a9
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-13 22:20:22 +0200

    sound player

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-function animals() {
-  const animalsContainer = document.querySelector(
-    '[data-js="animals-container"]'
-  );
+const animalButtons = document.querySelectorAll(".animalButton");
 
-  animalsContainer.append(animal);
-}
+animalButtons.forEach((animalButton) => {
+  animalButton.addEventListener("click", () => {
+    const animalClassNames = animalButton.className.split(" ");
+    const animalName = animalClassNames[1];
+    console.log(`The clicked Animal was ${animalName}`);
 
-animals();
+    const animalSound = new Audio(`assets/sounds/${animalName}.mp3`);
+
+    animalSound.play();
+    setTimeout(() => {
+      animalSound.pause();
+      animalSound.currentTime = 0;
+      console.log(`Audio for ${animalName} stopped after 1 seconds`);
+    }, 2000);
+  });
+});
