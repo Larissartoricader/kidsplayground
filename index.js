a4327c5112c1cc5052f66dc8abd38886d7aace7a
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-13 20:53:07 +0200

    animals icons

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ function animals() {
     '[data-js="animals-container"]'
   );
 
-  const animal = document.createElement("div");
-  animal.classList.add("animalButton");
-  animal.classList.add("bee");
   animalsContainer.append(animal);
 }
 
