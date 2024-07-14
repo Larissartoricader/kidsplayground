5c6935f4c9d0b70bb16afe944dbda81630c00c3f
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-07-14 20:10:21 +0200

    random animals in buttons

--- game.js
+++ game.js
@@ -1,5 +1,57 @@
 const startGame = document.querySelector(".start-button");
+const animalOne = document.querySelector(".animal1");
+const animalTwo = document.querySelector(".animal2");
+
+const animals = [
+  "bee",
+  "bird",
+  "cat",
+  "chick",
+  "cow",
+  "dolphin",
+  "donkey",
+  "ducky",
+  "hen",
+  "hippo",
+  "horse",
+  "lion",
+  "monkey",
+  "owl",
+  "parrot",
+  "penguin",
+  "pig",
+  "shark",
+  "sheep",
+  "tiger",
+];
 
 startGame.addEventListener("click", () => {
-  console.log("hello");
+  const randomButtons = [animalOne, animalTwo];
+
+  const rightButtonIndex = Math.floor(Math.random() * randomButtons.length);
+
+  console.log(rightButtonIndex);
+  const rightButton = randomButtons[rightButtonIndex];
+
+  const wrongButtonIndex = rightButtonIndex === 0 ? 1 : 0;
+  console.log(wrongButtonIndex);
+  const wrongButton = randomButtons[wrongButtonIndex];
+
+  console.log("These are all the buttons: " + randomButtons);
+  console.log(rightButton);
+  console.log(wrongButton);
+
+  const animalSecretIndex = Math.floor(Math.random() * animals.length);
+  const animalSecret = animals[animalSecretIndex];
+  console.log(animalSecret);
+  rightButton.style.backgroundImage = `url(./assets/images/${animalSecret}.png)`;
+
+  const animalRandomIndex = Math.floor(Math.random() * animals.length);
+  const animalRandom = animals[animalRandomIndex];
+  console.log(animalRandom);
+  wrongButton.style.backgroundImage = `url(./assets/images/${animalRandom}.png)`;
+
+  const playAnimalSecret = new Audio(`/assets/sounds/${animalSecret}.mp3`);
+
+  playAnimalSecret.play();
 });
